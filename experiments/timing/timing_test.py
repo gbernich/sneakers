2af0cd4b49c4de0292964bf698b55983f8ea971f
Garrett Bernichon <garrettbernichon@Garretts-MBP.home>
2018-03-02 16:31:36 -0500

    added comments and cleaned up print out

--- experiments/timing/timing_test.py
+++ experiments/timing/timing_test.py
@@ -5,7 +5,9 @@ import numpy
 
 # Define some constants
 NUM_TESTS      = 1000
-TIME_INCREMENT = 0.50  # seconds
+TIME_INCREMENT = 0.25  # seconds (this is arbitrary, but should be long enough
+                       # for the OS to context switch to a new process and then 
+                       # back to this process)
 
 
 # Initializations
@@ -28,9 +30,23 @@ for n in range(0, NUM_TESTS):
 	errors.append(diff)
 
 # Perform statistical analysis
-s = 'mean: %f seconds' % numpy.mean(errors)
+error_mean = numpy.mean(errors) * 1000
+error_std  = numpy.std(errors)  * 1000
+error_min  = numpy.min(errors)  * 1000
+error_max  = numpy.max(errors)  * 1000
+
+# Display results
+s = '\nRan %d tests: \n' % NUM_TESTS
 print(s)
 
-s = 'std:  %f seconds' % numpy.std(errors)
+s = 'mean: %f ms' % error_mean
 print(s)
 
+s = 'std:  %f ms' % error_std
+print(s)
+
+s = 'min:  %f ms' % error_min
+print(s)
+
+s = 'max:  %f ms\n' % error_max
+print(s)
