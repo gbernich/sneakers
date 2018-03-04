8a3740f6ea0afb66e6828d3d33f930a899efcdf5
Garrett Bernichon <garrettbernichon@Garretts-MBP.home>
2018-03-04 15:35:33 -0500

    initial commit of bash experimenting

---
+++ experiments/bash/launch_proc.sh
@@ -0,0 +1,19 @@
+# Usage:
+#
+# ./launch_proc.sh  <# of python proc>
+
+
+# Get Command line arguments
+args=("$@")
+
+NUM_PROC=${args[0]}
+
+# Read account information for each python process to be launched
+
+
+# Spawn multiple instances of Python with account info
+for n in `seq 1 $NUM_PROC`;
+do
+	echo $n
+	python test.py $n
+done
---
+++ experiments/bash/test.py
@@ -0,0 +1,5 @@
+import sys
+
+
+s = "Python instance: %d" % (int(sys.argv[1]))
+print(s) 
